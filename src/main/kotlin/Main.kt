4c673e8138f9aff02e3efbefebfe3251ff56b336
koman <twit13394@gmail.com>
2026-04-27 07:42:40 +0300

    Inital Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+
+fun main() {
+    val existing = 75
+    val newOrders = 12
+    val avgCheck = 2350.0
+    val totalOrders = existing + newOrders
+    val totalRevenue = totalOrders * avgCheck
+    println(totalOrders)
+    println(totalRevenue)
+}
