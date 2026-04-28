de6eb24bb6c6e6aeb0138814dd267c63c9e554c6
koman <twit13394@gmail.com>
2026-04-28 05:48:46 +0300

    Greeting quest

---
+++ src/main/kotlin/KS-3-1.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    val userName = "Иван"
+
+    // Объявляем переменные
+    var greeting: String = "Доброе утро"
+    println("$greeting, $userName!")
+
+
+    greeting = "Добрый вечер"
+    println("$greeting, $userName!")
+}
