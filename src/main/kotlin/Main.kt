2b3039d4ca880ffd2a599602964a30529d0049f0
koman <twit13394@gmail.com>
2026-04-28 17:00:53 +0300

    Задание 4-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,9 @@
 package org.example
 
 fun main() {
-    val input = "D2-D4;0"
-    val list = input.split("-", ";")
-    val from = list[0]
-    val to = list[1]
-    val stepNumber = list[2]
-    println(from)
-    println(to)
-    println(stepNumber)
+    val tables = 13
+    val todayBook = 13
+    val tomorrowBook = 9
+    println("Доступность столиков на сегодня: ${todayBook < tables}")
+    println("Достпуность столиков на завтра: ${tomorrowBook < tables}")
 }
