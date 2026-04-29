f5580e41c1e00968432ea797af4a9b5f3e99e5a5
koman <twit13394@gmail.com>
2026-04-29 16:02:36 +0300

    Задание 4-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
 package org.example
 
+const val REQUIRED_HUMIDITY = 20
+const val FORBIDDEN_SEASON = "зима"
+
 fun main() {
     val isSunnyWeather = true
     val isTentOpen = true
     val humidityPercent = 20
     val currentSeason = "зима"
 
-    val isFavorable = isSunnyWeather && isTentOpen && humidityPercent == 20 && currentSeason != "зима"
-    println("Благоприятные ли условия сейчас для роста бобовых? $isFavorable")
+    val isFavorable =
+        isSunnyWeather &&
+            isTentOpen &&
+            humidityPercent == REQUIRED_HUMIDITY &&
+            currentSeason != FORBIDDEN_SEASON
+
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? $isFavorable",
+    )
 }
