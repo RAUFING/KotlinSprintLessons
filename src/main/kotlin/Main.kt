617a97c1a646e1896bbc9141041b8f07b8a8dae1
koman <twit13394@gmail.com>
2026-04-29 06:07:07 +0300

    Задание 4-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,15 @@
 package org.example
 
-const val TABLES = 13
+const val AVERAGE_MIN_WEIGHT = 35
+const val AVERAGE_MAX_WEIGHT = 100
+const val AVERAGE_MAX_VOLUME = 100
 
 fun main() {
-    val todayBookedTables = 13
-    val tomorrowBookedTables = 9
-    println("[Доступность столиков на сегодня: ${todayBookedTables < TABLES}]\n[Доступность столиков на завтра: ${tomorrowBookedTables < TABLES}]")
+    val cargo1Weight = 20
+    val cargo1Volume = 80
+    val cargo2Weight = 50
+    val cargo2Volume = 100
+
+    println("Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории 'Average': ${cargo1Weight > AVERAGE_MIN_WEIGHT && cargo1Weight <= AVERAGE_MAX_WEIGHT && cargo1Volume < AVERAGE_MAX_VOLUME}")
+    println("Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории 'Average': ${cargo2Weight > AVERAGE_MIN_WEIGHT && cargo2Weight <= AVERAGE_MAX_WEIGHT && cargo2Volume < AVERAGE_MAX_VOLUME}")
 }
