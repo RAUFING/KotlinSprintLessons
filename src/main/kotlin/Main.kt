93b2bdbb2cd2a01407af12a9d922770dc9077578
koman <twit13394@gmail.com>
2026-04-29 15:54:09 +0300

    Задание 4-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,11 @@
 package org.example
 
-const val AVERAGE_MIN_WEIGHT = 35
-const val AVERAGE_MAX_WEIGHT = 100
-const val AVERAGE_MAX_VOLUME = 100
-
 fun main() {
-    val cargo1Weight = 20
-    val cargo1Volume = 80
-    val cargo2Weight = 50
-    val cargo2Volume = 100
+    val isSunnyWeather = true
+    val isTentOpen = true
+    val humidityPercent = 20
+    val currentSeason = "зима"
 
-    println(
-        "Груз с весом $cargo1Weight кг и объемом $cargo1Volume л соответствует категории 'Average': ${cargo1Weight > AVERAGE_MIN_WEIGHT && cargo1Weight <= AVERAGE_MAX_WEIGHT && cargo1Volume < AVERAGE_MAX_VOLUME}",
-    )
-    println(
-        "Груз с весом $cargo2Weight кг и объемом $cargo2Volume л соответствует категории 'Average': ${cargo2Weight > AVERAGE_MIN_WEIGHT && cargo2Weight <= AVERAGE_MAX_WEIGHT && cargo2Volume < AVERAGE_MAX_VOLUME}",
-    )
+    val isFavorable = isSunnyWeather && isTentOpen && humidityPercent == 20 && currentSeason != "зима"
+    println("Благоприятные ли условия сейчас для роста бобовых? $isFavorable")
 }
