1ec61a0f8aa55545d2d8f6fdc26f95720795e4b3
koman <twit13394@gmail.com>
2026-04-30 18:40:11 +0300

    Задание 4-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package org.example
 
+const val CREW_MIN = 55
+const val CREW_MAX = 70
+const val CREW_RECOMMENDED = 70
+const val PROVISION_MIN_BASIC = 50
+const val PROVISION_MIN_ALTERNATIVE = 50
+
 fun main() {
     val isDamaged = readln().toBoolean()
     val crewCount = readln().toInt()
@@ -8,6 +14,6 @@ fun main() {
 
     println(
         "Корабль может отправиться в плавание: " +
-            "${(!isDamaged && crewCount in 55..70 && provisionBoxes > 50 && (isWeatherGood || !isWeatherGood)) || (isDamaged && crewCount == 70 && isWeatherGood && provisionBoxes >= 50)}",
+            "${(!isDamaged && crewCount in CREW_MIN..CREW_MAX && provisionBoxes > PROVISION_MIN_BASIC) || (isDamaged && crewCount == CREW_RECOMMENDED && isWeatherGood && provisionBoxes >= PROVISION_MIN_ALTERNATIVE)}",
     )
 }
