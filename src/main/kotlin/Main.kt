3dcff16d0ab12608871d28303008d0969e2078ac
koman <twit13394@gmail.com>
2026-05-01 13:54:55 +0300

    Задание 5-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ const val AGE_OF_MAJORITY = 18
 
 fun main() {
     val currentYear = 2026
-    val input = readln().toInt()
-    val age = currentYear - input
+    val yearBorn = readln().toInt()
+    val age = currentYear - yearBorn
     if (age >= AGE_OF_MAJORITY) {
         println("Показать экран со скрытым контентом")
     } else {
