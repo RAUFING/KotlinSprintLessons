7fa90813b63867b30bb51ae988bf644a258a6865
koman <twit13394@gmail.com>
2026-05-01 13:37:15 +0300

    Задание 5-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,13 @@
 package org.example
 
-const val CREW_MIN = 55
-const val CREW_MAX = 70
-const val CREW_RECOMMENDED = 70
-const val PROVISION_MIN_BASIC = 50
-const val PROVISION_MIN_ALTERNATIVE = 50
+const val CORRECT_ANSWER = 4
 
 fun main() {
-    val isDamaged = readln().toBoolean()
-    val crewCount = readln().toInt()
-    val provisionBoxes = readln().toInt()
-    val isWeatherGood = readln().toBoolean()
-
-    println(
-        "Корабль может отправиться в плавание: " +
-            "${(!isDamaged && crewCount in CREW_MIN..CREW_MAX && provisionBoxes > PROVISION_MIN_BASIC) || (isDamaged && crewCount == CREW_RECOMMENDED && isWeatherGood && provisionBoxes >= PROVISION_MIN_ALTERNATIVE)}",
-    )
+    println("Уважаемый пользователь! Докажите что вы не бот. Решите пример: 2+2")
+    val input = readln().toInt()
+    if (input == CORRECT_ANSWER) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
 }
