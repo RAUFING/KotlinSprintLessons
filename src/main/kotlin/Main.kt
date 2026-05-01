e962ac4c8022f5c08f4e6c2e1c9bc2cbc65c5a31
koman <twit13394@gmail.com>
2026-05-01 14:07:27 +0300

    Задание 5-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,24 @@
 package org.example
 
-const val AGE_OF_MAJORITY = 18
-
 fun main() {
-    val currentYear = 2026
-    val yearBorn = readln().toInt()
-    val age = currentYear - yearBorn
-    if (age >= AGE_OF_MAJORITY) {
-        println("Показать экран со скрытым контентом")
+    val number1 = 12
+    val number2 = 56
+
+    println("Введите первое число:")
+    val userNumber1 = readln().toInt()
+    println("Введите второе число:")
+    val userNumber2 = readln().toInt()
+
+    val guessedFirst = userNumber1 == number1 || userNumber1 == number2
+    val guessedSecond = userNumber2 == number1 || userNumber2 == number2
+
+    if (guessedFirst && guessedSecond) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (guessedFirst || guessedSecond) {
+        println("Вы выиграли утешительный приз!")
     } else {
-        println("Доступ ограничен")
+        println("Неудача!")
     }
+
+    println("Для победы нужно было угадать числа: $number1 и $number2")
 }
