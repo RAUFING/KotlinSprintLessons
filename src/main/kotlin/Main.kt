e62429dba0ecefef575268ac2e3ca0d10303ec3a
koman <twit13394@gmail.com>
2026-05-02 09:27:55 +0300

    Задание 6-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,16 +6,20 @@ fun main() {
     println("Придумайте пароль:")
     val password = readln()
     println("Отлично, теперь войдите")
-    while (true) {
+
+    var inputLogin: String
+    var inputPassword: String
+
+    do {
         println("Введите логин:")
-        val inputLogin = readln()
+        inputLogin = readln()
         println("Введите пароль:")
-        val inputPassword = readln()
-        if (inputLogin == login && inputPassword == password) {
-            break
-        } else {
+        inputPassword = readln()
+
+        if (inputLogin != login || inputPassword != password) {
             println("Данные не верны, попробуйте еще раз")
         }
-    }
+    } while (inputLogin != login || inputPassword != password)
+
     println("Авторизация прошла успешно")
 }
