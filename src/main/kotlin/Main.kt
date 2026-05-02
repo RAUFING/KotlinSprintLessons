267bfc9b54b630f1eb35acf6193785f88f058abd
koman <twit13394@gmail.com>
2026-05-02 09:50:37 +0300

    Задание 6-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,8 @@ package org.example
 fun main() {
     println("Сколько секунд вы хотите засечь?")
     val userSeconds = readln().toInt()
-    var remainingSeconds = userSeconds
 
-    while (remainingSeconds > 0) {
-        remainingSeconds--
-        Thread.sleep(1000)
-    }
+    Thread.sleep(userSeconds * 1000L)
 
     println("Прошло $userSeconds секунд")
 }
