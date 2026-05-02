31d6b1086e498eab9cb641d4e70d13f7879f8fcf
koman <twit13394@gmail.com>
2026-05-02 23:12:37 +0300

    Задание 6-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,24 @@
 package org.example
 
 fun main() {
-    println("Угадай число от 1 до 9")
-    val number = (1..9).random()
-    var attempts = 5
+    var attempts = 3
 
     while (attempts > 0) {
-        println("Введите число:")
-        val guess = readln().toInt()
-        attempts--
-        if (guess != number) {
-            println("Неверно, осталось попыток: $attempts")
-        } else {
-            println("Это была великолепная игра!")
+        val number1 = (1..9).random()
+        val number2 = (1..9).random()
+        val correctAnswer = number1 + number2
+        println("Решите пример: $number1 + $number2")
+        val userAnswer = readln().toInt()
+        if (userAnswer == correctAnswer) {
+            println("Добро пожаловать!")
             return
+        } else {
+            attempts--
+            if (attempts > 0) {
+                println("Неверно, осталось попыток: $attempts")
+            }
         }
     }
 
-    println("Было загадано число $number")
+    println("Доступ запрещен")
 }
