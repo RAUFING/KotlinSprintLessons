0252ab77be3ef1ad8bd534cb8205153ad0c9cf3d
koman <twit13394@gmail.com>
2026-05-03 16:45:08 +0300

    Задание 7-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,17 @@
 package org.example
 
 fun main() {
-    var attempts = 3
+    val symbols = 'a'..'z'
+    val numbers = '0'..'9'
+    var pin = ""
 
-    while (attempts > 0) {
-        val number1 = (1..9).random()
-        val number2 = (1..9).random()
-        println("Решите пример: $number1 + $number2")
-        val userAnswer = readln().toInt()
-
-        if (userAnswer == number1 + number2) {
-            println("Добро пожаловать!")
-            return
-        } else {
-            attempts--
-
-            if (attempts > 0) {
-                println("Неверно, осталось попыток: $attempts")
-            }
+    for (i in 1..6) {
+        if (i % 2 == 0) {
+            pin += numbers.random()
+            continue
         }
+        pin += symbols.random()
     }
 
-    println("Доступ запрещен")
+    println(pin)
 }
