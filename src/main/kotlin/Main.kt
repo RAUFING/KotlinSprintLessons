9ec6e2b0588ddd107068ce47a219eb339f86cbde
koman <twit13394@gmail.com>
2026-05-03 17:32:38 +0300

    Задание 7-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 package org.example
 
+const val PASSWORD_SIZE = 6
+
 fun main() {
     val symbols = 'a'..'z'
     val numbers = '0'..'9'
     var pin = ""
 
-    for (i in 1..6) {
-        if (i % 2 == 0) {
-            pin += numbers.random()
-            continue
-        }
-        pin += symbols.random()
+    for (i in 1..PASSWORD_SIZE) {
+        pin +=
+            if (i % 2 == 0) {
+                numbers.random()
+            } else {
+                symbols.random()
+            }
     }
 
     println(pin)
