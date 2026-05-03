d24312f95885872124e68aa66de406c4f5f9af2f
koman <twit13394@gmail.com>
2026-05-03 21:56:15 +0300

    Задание 7-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,9 @@
 package org.example
 
 fun main() {
-    var code: Int
-    var userInput: Int
-
-    do {
-        code = (1000..9999).random()
-        println("Ваш код авторизации: $code")
-        userInput = readln().toInt()
-    } while (userInput != code)
-
-    println("Добро пожаловать!")
+    println("Введите число:")
+    val userInput = readln().toInt()
+    for (i in 0..userInput step 2) {
+        println(i)
+    }
 }
