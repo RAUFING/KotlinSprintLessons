57d0e150d6bafb642d339d9488869475b73f96de
koman <twit13394@gmail.com>
2026-05-03 18:29:04 +0300

    Задание 7-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,13 @@
 package org.example
 
-const val PASSWORD_SIZE = 6
-
 fun main() {
-    val symbols = 'a'..'z'
-    val numbers = '0'..'9'
-    var password = ""
-
-    for (i in 1..PASSWORD_SIZE) {
-        password +=
-            if (i % 2 == 0) {
-                numbers.random()
-            } else {
-                symbols.random()
-            }
+    while (true) {
+        val code = (1000..9999).random()
+        println("Ваш код авторизации: $code")
+        val userInput = readln().toInt()
+        if (userInput == code) {
+            println("Добро пожаловать!")
+            break
+        }
     }
-
-    println(password)
 }
