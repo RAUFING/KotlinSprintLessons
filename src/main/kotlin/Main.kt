52f7af0af539634702e2181d2164cde4d200d03e
koman <twit13394@gmail.com>
2026-05-04 15:45:13 +0300

    Задание 8-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,6 @@
 package org.example
 
-const val MIN_PASSWORD_LENGTH = 6
-
 fun main() {
-    println("Введите длину пароля (минимум $MIN_PASSWORD_LENGTH символов):")
-    val length = readln().toIntOrNull() ?: MIN_PASSWORD_LENGTH
-    val passwordLength = if (length < MIN_PASSWORD_LENGTH) MIN_PASSWORD_LENGTH else length
-
-    val digits = '0'..'9'
-    val lowercaseLetters = 'a'..'z'
-    val uppercaseLetters = 'A'..'Z'
-    val allSymbols = digits + lowercaseLetters + uppercaseLetters
-
-    var password = ""
-
-    password += digits.random()
-    password += lowercaseLetters.random()
-    password += uppercaseLetters.random()
-
-    for (i in 4..passwordLength) {
-        password += allSymbols.random()
-    }
-
-    val shuffledPassword = password.toList().shuffled().joinToString("")
-    println("Ваш пароль: $shuffledPassword")
+    val adsViewsWeek = arrayOf(1200, 200, 500, 300, 900, 2000, 1900)
+    println(adsViewsWeek.sum())
 }
