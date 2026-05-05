d8e45d86f920167521d77cf39cb88d22ca408433
koman <twit13394@gmail.com>
2026-05-05 08:55:09 +0300

    Задание 8-4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,12 @@ fun main() {
     val ingredients = arrayOf("мука", "сахар", "яйца", "молоко", "масло")
     println(ingredients.joinToString(", "))
     println("Какой ингридиент вы хотите удалить?")
-    val query = readln()
-
+    val queryIngredient = readln()
+    if (queryIngredient in ingredients) {
+        val queryReplaceIngredient = readln()
+        ingredients[ingredients.indexOf(queryIngredient)] = queryReplaceIngredient
+    } else {
+        println("Такого ингредиента нет в списке")
+    }
+    println("Готов! Вы сохранили следущий список: [${ingredients.joinToString(", ")}]")
 }
