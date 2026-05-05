98303921593e44359d7596dc6a4f1cb8834bf45e
koman <twit13394@gmail.com>
2026-05-05 08:31:44 +0300

    Задание 8-4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,8 @@ package org.example
 
 fun main() {
     val ingredients = arrayOf("мука", "сахар", "яйца", "молоко", "масло")
-    println("Введите название ингредиента:")
+    println(ingredients.joinToString(", "))
+    println("Какой ингридиент вы хотите удалить?")
     val query = readln()
-    if (query in ingredients) {
-        println("Ингредиент $query в рецепте есть")
-    } else {
-        println("Такого ингредиента в рецепте нет")
-    }
+
 }
