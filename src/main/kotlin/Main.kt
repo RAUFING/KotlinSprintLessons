24508c869af84db0ca911660e135446f3fa46ab9
koman <twit13394@gmail.com>
2026-05-06 18:17:25 +0300

    Задание 9-4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,9 @@
 package org.example
 
 fun main() {
-    val omeletteForOne = listOf(2, 50, 15)
-    println("Введите количество порций:")
-    val portions = readln().toInt()
-
-    println(
-        "На $portions порций вам понадобится: " +
-                "Яиц – ${omeletteForOne[0] * portions}, " +
-                "молока – ${omeletteForOne[1] * portions}, " +
-                "сливочного масла – ${omeletteForOne[2] * portions}"
-    )
+    println("Введите 5 ингредиентов через запятую с пробелом:")
+    val input = readln()
+    val ingredients = input.split(", ").toMutableList()
+    val sortedIngredients = ingredients.sorted()
+    println("Отсортированный список: ${sortedIngredients.joinToString(", ")}")
 }
