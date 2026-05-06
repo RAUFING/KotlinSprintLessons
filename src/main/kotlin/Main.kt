97a660a76df91425ca7c98c4129d464c4301129a
koman <twit13394@gmail.com>
2026-05-06 18:13:09 +0300

    Задание 9-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
 package org.example
 
 fun main() {
-    val ingredients = listOf("мука", "яйца", "молоко")
-    println("В рецепте есть следующие ингредиенты: ${ingredients.joinToString(", ")}")
-    ingredients.forEach { println(it) }
+    val baseIngredients = mutableListOf("мука", "яйца", "молоко")
+    println("В рецепте есть базовые ингредиенты: [${baseIngredients.joinToString(", ")}]")
+
+    println("Желаете добавить еще?")
+    val answer = readln()
+
+    if (!answer.equals("да", ignoreCase = true)) {
+        return
+    }
+
+    println("Какой ингредиент вы хотите добавить?")
+    val newIngredient = readln()
+    baseIngredients.add(newIngredient)
+
+    println("Теперь в рецепте есть следующие ингредиенты: [${baseIngredients.joinToString(", ")}]")
 }
