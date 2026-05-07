364845da06a54df1a774731d7ec13c98f84b929f
koman <twit13394@gmail.com>
2026-05-07 15:14:24 +0300

    Задание 9-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,12 @@ fun main() {
     val omeletteForOne = listOf(2, 50, 15)
     println("Введите количество порций:")
     val portions = readln().toInt()
+    val neededIngredients = omeletteForOne.map { it * portions }
 
     println(
         "На $portions порций вам понадобится: " +
-                "Яиц – ${omeletteForOne[0] * portions}, " +
-                "молока – ${omeletteForOne[1] * portions}, " +
-                "сливочного масла – ${omeletteForOne[2] * portions}"
+                "Яиц – ${neededIngredients[0]}, " +
+                "молока – ${neededIngredients[1]}, " +
+                "сливочного масла – ${neededIngredients[2]}"
     )
 }
