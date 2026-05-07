2f33d23f4f522cf0542d7b3cd70e577a33d67ebc
koman <twit13394@gmail.com>
2026-05-07 15:16:26 +0300

    Задание 9-4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,6 @@ package org.example
 fun main() {
     println("Введите 5 ингредиентов через запятую с пробелом:")
     val input = readln()
-    val ingredients = input.split(", ").toMutableList()
-    val sortedIngredients = ingredients.sorted()
-    println("Отсортированный список: ${sortedIngredients.joinToString(", ")}")
+    val ingredients = input.split(", ").sorted()
+    println("Отсортированный список: ${ingredients.joinToString(", ")}")
 }
