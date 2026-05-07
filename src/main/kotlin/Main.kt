765fecb66477a5bbe61a84b5e6ee3ef0109f711c
koman <twit13394@gmail.com>
2026-05-07 15:11:20 +0300

    Задание 8-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,9 @@ package org.example
 fun main() {
     println("Введите количество ингредиентов:")
     val size = readln().toInt()
-    val ingredients = arrayOfNulls<String>(size)
+    val ingredients = Array(size) { "" }
 
-    for (i in 0 until size) {
+    for (i in ingredients.indices) {
         println("Введите ингредиент ${i + 1}:")
         ingredients[i] = readln()
     }
