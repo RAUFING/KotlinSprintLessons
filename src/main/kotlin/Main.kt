7bd38115b5760fc4a2ccb1d7b3112066351b178f
koman <twit13394@gmail.com>
2026-05-09 05:35:58 +0300

    Задание 11-1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+package org.example
+
+class User(
+    val id: Long,
+    val login: String,
+    val password: String,
+    val email: String
+)
+
+fun main() {
+    val user1 = User(
+        id = 1,
+        login = "android_dev",
+        password = "qwerty123",
+        email = "android@example.com"
+    )
+    val user2 = User(
+        id = 2,
+        login = "kotlin_fan",
+        password = "securePass!",
+        email = "kotlin@example.com"
+    )
+
+    println("ID: ${user1.id}, Логин: ${user1.login}, Пароль: ${user1.password}, Почта: ${user1.email}")
+    println("ID: ${user2.id}, Логин: ${user2.login}, Пароль: ${user2.password}, Почта: ${user2.email}")
+}
--- src/main/kotlin/lesson11/Main.kt
+++
@@ -1,7 +0,0 @@
-package lesson11
-
-fun main() {
-    val user1 = User(1, "Misha47", "5EWK4efffu", "misha@4chan.com")
-    val user2 = User(2, "Danya228", "ug85n394", "danyachan@4chan.com")
-    println("Данные пользователя ${user1.id}: логин - ${user1.login}, пароль - ${user1.password}, email - ${user1.email}")
-}
