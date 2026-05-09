4e21f292fd3ea2cc61fb610fa3daf20a8e99971c
koman <twit13394@gmail.com>
2026-05-09 05:40:59 +0300

    Задание 11-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,21 +4,20 @@ class User(
     val id: Long,
     val login: String,
     val password: String,
-    val email: String
-)
+    val email: String,)
 
 fun main() {
     val user1 = User(
         id = 1,
         login = "android_dev",
         password = "qwerty123",
-        email = "android@example.com"
+        email = "android@example.com",
     )
     val user2 = User(
         id = 2,
         login = "kotlin_fan",
         password = "securePass!",
-        email = "kotlin@example.com"
+        email = "kotlin@example.com",
     )
 
     println("ID: ${user1.id}, Логин: ${user1.login}, Пароль: ${user1.password}, Почта: ${user1.email}")
