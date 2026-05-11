bc38a65382abf4264f33d02eb19386e11f6b2db1
koman <twit13394@gmail.com>
2026-05-11 08:03:20 +0300

    Задание 12-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 package org.example
 
-class WeatherForecast {
-    var daytimeTemperature: Int = 0
-    var nighttimeTemperature: Int = 0
-    var isPrecipitation: Boolean = false
-
+class WeatherData(
+    val daytimeTemperature: Int,
+    val nighttimeTemperature: Int,
+    val isPrecipitation: Boolean
+) {
     fun printInfo() {
         println("Дневная температура: $daytimeTemperature")
         println("Ночная температура: $nighttimeTemperature")
@@ -13,15 +13,10 @@ class WeatherForecast {
 }
 
 fun main() {
-    val day1 = WeatherForecast()
-    day1.daytimeTemperature = 25
-    day1.nighttimeTemperature = 15
-    day1.isPrecipitation = false
-    day1.printInfo()
-
-    val day2 = WeatherForecast()
-    day2.daytimeTemperature = 10
-    day2.nighttimeTemperature = 3
-    day2.isPrecipitation = true
-    day2.printInfo()
+    val day = WeatherData(
+        daytimeTemperature = 25,
+        nighttimeTemperature = 15,
+        isPrecipitation = false
+    )
+    day.printInfo()
 }
