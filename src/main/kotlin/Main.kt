5029c8a0f239cb4b2f83b5775d97c6e37cbb6fd3
koman <twit13394@gmail.com>
2026-05-12 07:07:51 +0300

    Задание 13-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
 package org.example
 
-class PhoneBookEntry(
+class PhoneBookContact(
     val name: String,
     val phoneNumber: Long,
-    val company: String?,
-)
+    val company: String? = null,
+) {
+    fun printInfo() {
+        println(
+            "Имя: $name\n" +
+            "Номер: $phoneNumber\n" +
+            "Компания: ${company ?: "<не указано>"}"
+        )
+    }
+}
+
+fun main() {
+    val contact = PhoneBookContact(
+        name = "Ростислав",
+        phoneNumber = 89123456789,
+    )
+    contact.printInfo()
+}
