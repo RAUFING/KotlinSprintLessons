a445935a218b84f3c3a3cb3caab2834e3b9e6e33
koman <twit13394@gmail.com>
2026-05-14 23:14:56 +0300

    Задание 15-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,94 @@
 package org.example
 
-interface Swimmable {
-    fun swimming()
+interface Movable {
+    fun move()
 }
 
-interface Flyable {
-    fun flying()
-}
+interface PassengerTransportable {
+    val maxPassengers: Int
+    var currentPassengers: Int
 
-class CrucianCarp : Swimmable {
-    override fun swimming() {
-        println("Карась плавает в воде")
+    fun loadPassenger() {
+        if (currentPassengers < maxPassengers) {
+            currentPassengers++
+            println("Пассажир загружен. Текущее количество: $currentPassengers/$maxPassengers")
+        } else {
+            println("Нет мест для пассажиров")
+        }
+    }
+
+    fun unloadPassenger() {
+        if (currentPassengers > 0) {
+            currentPassengers--
+            println("Пассажир выгружен. Текущее количество: $currentPassengers/$maxPassengers")
+        } else {
+            println("Нет пассажиров для выгрузки")
+        }
     }
 }
 
-class Gull : Flyable {
-    override fun flying() {
-        println("Чайка летит над волнами")
+interface CargoTransportable {
+    val maxCargo: Double
+    var currentCargo: Double
+
+    fun loadCargo(amount: Double) {
+        if (currentCargo + amount <= maxCargo) {
+            currentCargo += amount
+            println("Груз загружен. Текущий вес: $currentCargo/$maxCargo тонн")
+        } else {
+            println("Недостаточно места для груза")
+        }
+    }
+
+    fun unloadCargo(amount: Double) {
+        if (currentCargo >= amount) {
+            currentCargo -= amount
+            println("Груз выгружен. Текущий вес: $currentCargo/$maxCargo тонн")
+        } else {
+            println("Недостаточно груза для выгрузки")
+        }
     }
 }
 
-class Duck : Swimmable, Flyable {
-    override fun swimming() {
-        println("Утка плавает в пруду")
-    }
+class Truck : Movable, PassengerTransportable, CargoTransportable {
+    override val maxPassengers = 1
+    override var currentPassengers = 0
+    override val maxCargo = 2.0
+    override var currentCargo = 0.0
 
-    override fun flying() {
-        println("Утка летит на зимовку")
+    override fun move() {
+        println("Грузовик движется")
+    }
+}
+
+class Car : Movable, PassengerTransportable {
+    override val maxPassengers = 3
+    override var currentPassengers = 0
+
+    override fun move() {
+        println("Легковой автомобиль движется")
     }
 }
 
 fun main() {
-    val carp = CrucianCarp()
-    carp.swimming()
+    val truck = Truck()
+    val car1 = Car()
+    val car2 = Car()
 
-    val gull = Gull()
-    gull.flying()
+    truck.loadCargo(2.0)
 
-    val duck = Duck()
-    duck.swimming()
-    duck.flying()
+    truck.loadPassenger()
+
+    car1.loadPassenger()
+    car1.loadPassenger()
+    car1.loadPassenger()
+
+    car2.loadPassenger()
+    car2.loadPassenger()
+    car2.loadPassenger()
+
+    println("\nИтог:")
+    println("Грузовик: пассажиры ${truck.currentPassengers}/${truck.maxPassengers}, груз ${truck.currentCargo}/${truck.maxCargo} тонн")
+    println("Легковая 1: пассажиры ${car1.currentPassengers}/${car1.maxPassengers}")
+    println("Легковая 2: пассажиры ${car2.currentPassengers}/${car2.maxPassengers}")
 }
