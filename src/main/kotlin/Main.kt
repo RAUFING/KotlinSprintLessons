75ccaa0de4e4f8709048f15b69391c3590c1afce
koman <twit13394@gmail.com>
2026-05-16 09:01:21 +0300

    Задание 16-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
 package org.example
 
-class Dice {
-    private val number = (1..6).random()
-    fun printNumber() = println(number)
+class Circle(private val radius: Double) {
+    private val pi = 3.14
+
+    fun circumference(): Double = 2 * pi * radius
+    fun area(): Double = pi * radius * radius
+}
+
+fun main() {
+    val circle = Circle(radius = 5.0)
+    println("Длина окружности: ${circle.circumference()}")
+    println("Площадь круга: ${circle.area()}")
 }
