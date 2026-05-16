6b0c00d50205e9ad240582f23f48cb2d28d8ca99
koman <twit13394@gmail.com>
2026-05-16 20:40:57 +0300

    Задание 16-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,5 @@
 package org.example
 
-import kotlin.math.pow
-
-private const val PI = 3.14
-
-class Circle(private val radius: Double) {
-    fun getCircumference(): Double = 2 * PI * radius
-    fun getArea(): Double = PI * radius.pow(2)
-}
-
-fun main() {
-    val circle = Circle(5.0)
-    println("Длина окружности: ${circle.getCircumference()}")
-    println("Площадь круга: ${circle.getArea()}")
+class User(val login: String, private val password: String) {
+    fun validatePassword(input: String): Boolean = input == password
 }
