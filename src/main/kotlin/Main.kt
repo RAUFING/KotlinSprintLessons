3ebc499f87e1cd38af3b3683a616d30a81cfd039
koman <twit13394@gmail.com>
2026-05-16 15:07:21 +0300

    Задание 16-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,10 @@ package org.example
 
 class Dice {
     private val number = (1..6).random()
-    fun printNumber() = println(number)
+    fun printNumber() = println("На кубике выпало $number")
+}
+
+fun main() {
+    val dice = Dice()
+    dice.printNumber()
 }
