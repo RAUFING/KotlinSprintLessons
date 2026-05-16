3bbc5486fbd19ed0bd98e8eaa293fde6743ef345
koman <twit13394@gmail.com>
2026-05-16 21:18:17 +0300

    Задание 16-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 package org.example
 
-class User(val login: String, private val password: String) {
+class User(private val login: String, private val password: String) {
     fun validatePassword(input: String): Boolean = input == password
+}
+
+fun main() {
+    val user = User("rauf", "qwerty123")
+    println(user.validatePassword("wrong"))
+    println(user.validatePassword("qwerty123"))
 }
