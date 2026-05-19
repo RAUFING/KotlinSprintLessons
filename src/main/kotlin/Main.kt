3cf80cd41837aa7a0d5532f3dd052d7d2dd5eeee
koman <twit13394@gmail.com>
2026-05-19 22:35:35 +0300

    Задание 18-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
 package org.example
 
 class Screen {
+    fun drawCircle(x: Int, y: Int): String = "Нарисован круг с центром ($x, $y)"
+    fun drawCircle(x: Float, y: Float): String = "Нарисован круг с центром ($x, $y)"
+    fun drawSquare(x: Int, y: Int): String = "Нарисован квадрат с углом ($x, $y)"
+    fun drawSquare(x: Float, y: Float): String = "Нарисован квадрат с углом ($x, $y)"
     fun drawPoint(x: Int, y: Int): String = "Нарисована точка с координатами ($x, $y)"
     fun drawPoint(x: Float, y: Float): String = "Нарисована точка с координатами ($x, $y)"
-    fun drawCircle(x: Int, y: Int, radius: Int): String = "Нарисован круг с центром ($x, $y) и радиусом $radius"
-    fun drawCircle(x: Float, y: Float, radius: Float): String = "Нарисован круг с центром ($x, $y) и радиусом $radius"
-    fun drawSquare(x: Int, y: Int, width: Int, height: Int): String = "Нарисован квадрат с углом ($x, $y), шириной $width и высотой $height"
-    fun drawSquare(x: Float, y: Float, width: Float, height: Float): String = "Нарисован квадрат с углом ($x, $y), шириной $width и высотой $height"
 }
 
 fun main() {
     val screen = Screen()
     println(screen.drawPoint(10, 20))
     println(screen.drawPoint(10.5f, 20.5f))
-    println(screen.drawCircle(5, 5, 10))
-    println(screen.drawCircle(5.5f, 5.5f, 10.5f))
-    println(screen.drawSquare(0, 0, 30, 40))
-    println(screen.drawSquare(0.0f, 0.0f, 30.5f, 40.5f))
+    println(screen.drawCircle(5, 5))
+    println(screen.drawCircle(5.5f, 5.5f))
+    println(screen.drawSquare(0, 0))
+    println(screen.drawSquare(0.0f, 0.0f))
 }
