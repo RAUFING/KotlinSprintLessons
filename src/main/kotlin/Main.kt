5048411ecd58637073a17f85984730885ed349aa
koman <twit13394@gmail.com>
2026-05-20 11:01:44 +0300

    Задание 19-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 package org.example
 
-enum class FishType {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class FishType(val fishName: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
     println("Вы можете добавить в аквариум следующих рыб:")
     for (fish in FishType.entries) {
-        println("- ${fish.name}")
+        println("- ${fish.fishName}")
     }
 }
