7618332b607958fcd814807219313b1273928989
koman <twit13394@gmail.com>
2026-05-22 00:08:21 +0300

    Задание 20-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
 package org.example
 
-fun main(){
-    val greet: (String) -> String = { username -> "С наступающим Новым Годом, ${username}!" }
-    println(greet("Анна"))
+class Player(
+    val name: String,
+    val maxHealth: Int,
+    var currentHealth: Int,
+)
+
+fun main() {
+    val healPotion: (Player) -> Unit = { player ->
+        player.currentHealth = player.maxHealth
+        println("${player.name} выпил лечебное зелье! Здоровье восстановлено до ${player.currentHealth}/${player.maxHealth}")
+    }
+
+    val player = Player("Герой", 100, 30)
+    println("До лечения: ${player.currentHealth}/${player.maxHealth}")
+    healPotion(player)
 }
