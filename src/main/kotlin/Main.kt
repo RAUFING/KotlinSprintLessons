6b2705d599a5ac9380f3af926c5c3aecd89f67b2
koman <twit13394@gmail.com>
2026-05-22 00:17:55 +0300

    Задание 20-5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,40 @@
 package org.example
 
-fun main() {
-    val items = listOf("Кнопка", "Меню", "Профиль", "Настройки", "Помощь")
+class Robot {
+    private val phrases = listOf(
+        "Привет человек",
+        "Я робот",
+        "Сегодня хорошая погода",
+        "Мне нужна зарядка",
+        "Давай работать",
+    )
 
-    val clickActions: List<() -> Unit> = items.map { item ->
-        { println("Нажат элемент $item") }
-    }
+    private var modifier: ((String) -> String)? = null
 
-    for (i in items.indices) {
-        if (i % 2 == 0) {
-            clickActions[i]()
+    fun say() {
+        val phrase = phrases.random()
+        val result = if (modifier != null) {
+            modifier!!(phrase)
+        } else {
+            phrase
         }
+        println(result)
     }
+
+    fun setModifier(mod: (String) -> String) {
+        modifier = mod
+    }
+}
+
+fun main() {
+    val robot = Robot()
+
+    println("Обычная речь:")
+    robot.say()
+
+    println("\nУстанавливаем инвертирующий модификатор...")
+    robot.setModifier { phrase -> phrase.reversed() }
+
+    println("Речь с модификатором:")
+    robot.say()
 }
