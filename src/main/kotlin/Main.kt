bcd2986b444a1f0d0bf5fb32997d1d1e0a359653
koman <twit13394@gmail.com>
2026-05-22 22:18:06 +0300

    Задание 21-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,12 @@
 package org.example
 
-class Robot {
-    private val phrases = listOf(
-        "Привет человек",
-        "Я робот",
-        "Сегодня хорошая погода",
-        "Мне нужна зарядка",
-        "Давай работать",
-    )
-
-    private var modifier: (String) -> String = { it }
-
-    fun say() {
-        val phrase = phrases.random()
-        println(modifier(phrase))
-    }
-
-    fun setModifier(mod: (String) -> String) {
-        modifier = mod
-    }
+fun String.vowelCount(): Int {
+    val vowels = setOf('a', 'e', 'i', 'o', 'u', 'y', 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я')
+    return count { it.lowercaseChar() in vowels }
 }
 
 fun main() {
-    val robot = Robot()
-
-    println("Обычная речь:")
-    robot.say()
-
-    println("\nУстанавливаем инвертирующий модификатор...")
-    robot.setModifier { phrase -> phrase.reversed() }
-
-    println("Речь с модификатором:")
-    robot.say()
+    println("hello".vowelCount())
+    println("Kotlin".vowelCount())
+    println("Привет".vowelCount())
 }
