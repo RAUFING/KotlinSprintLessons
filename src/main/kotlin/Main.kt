d274fc09c37067aa3e340141c0d5038f47a9b43c
koman <twit13394@gmail.com>
2026-05-23 23:05:43 +0300

    Задание 22-4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,24 @@
 package org.example
 
-data class User(
-    val name: String,
-    val age: Int,
-    val email: String,
+data class MainScreenState(
+    val data: String = "",
+    val isLoading: Boolean = false,
 )
 
+class MainScreenViewModel {
+    var mainScreenState: MainScreenState = MainScreenState()
+        private set
+
+    fun loadData() {
+        mainScreenState = MainScreenState(isLoading = true)
+        mainScreenState = MainScreenState(data = "Данные с сервера загружены", isLoading = false)
+    }
+}
+
 fun main() {
-    val user = User("Анна", 25, "anna@example.com")
+    val viewModel = MainScreenViewModel()
+    println("Начальное состояние: ${viewModel.mainScreenState}")
 
-    val (name, age, email) = user
-
-    println("Имя: $name")
-    println("Возраст: $age")
-    println("Email: $email")
+    viewModel.loadData()
+    println("Конечное состояние: ${viewModel.mainScreenState}")
 }
