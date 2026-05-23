ee8749f7c8ed4d6390e6d72ad30eadaa05a24f75
koman <twit13394@gmail.com>
2026-05-23 23:02:11 +0300

    Задание 22-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,24 @@
 package org.example
 
-class RegularBook(
+class RegularBookV2(
     val title: String,
     val author: String,
 )
 
-data class DataBook(
+data class DataBookV2(
     val title: String,
     val author: String,
 )
 
 fun main() {
-    val regular1 = RegularBook("Война и мир", "Толстой")
-    val regular2 = RegularBook("Война и мир", "Толстой")
+    val regular = RegularBookV2("Война и мир", "Толстой")
+    val data = DataBookV2("Война и мир", "Толстой")
 
-    val data1 = DataBook("Война и мир", "Толстой")
-    val data2 = DataBook("Война и мир", "Толстой")
+    // Обычный класс по умолчанию использует toString() из Any,
+    // который выводит имя класса и хеш-код объекта (адрес в памяти).
+    println(regular)
 
-    // Обычный класс сравнивает ссылки на объекты (адреса в памяти), а не содержимое.
-    // Поэтому два разных объекта с одинаковыми данными НЕ равны.
-    println("RegularBook: ${regular1 == regular2}")
-
-    // Data class автоматически переопределяет equals() и сравнивает содержимое объектов.
-    // Поэтому два объекта с одинаковыми данными равны.
-    println("DataBook: ${data1 == data2}")
+    // Data class автоматически переопределяет toString()
+    // и выводит значения всех свойств в читаемом формате.
+    println(data)
 }
